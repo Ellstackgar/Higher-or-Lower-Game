d40a44c5e17ddc4c5fd88d46870a3004fbc97c33
Ellstackgar <13chanb@gmail.com>
2026-02-12 21:26:48 +0000

    Add files via upload

---
+++ Higher or Lower/higher-or-lower.js
@@ -0,0 +1,64 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+let generated = Math.floor(Math.random()*1000) + 1;
+
+function gameCycle() {
+
+    rl.question("Pick any number. The chosen number is between 1 and 1000:\n", (num) => {
+    
+        let guess = Number(num);
+
+            if (isNaN(guess)) {
+
+                console.log("That isn't a valid number!");
+            
+                gameCycle();
+
+
+            } else {
+        
+                if (guess > generated) {
+
+                    console.log("The number is lower.")
+                    
+                    gameCycle();
+    
+                 } else if (guess < generated) {
+
+                console.log("The number is higher.")
+
+                gameCycle();
+
+                 }
+
+             else {
+        
+                console.log("Well done! You have guessed the right number!")
+
+                rl.close(); //change this line to being when user exits menu
+           
+
+        }
+        }    
+    
+        });
+    
+
+    
+    };
+
+
+
+
+ 
+
+gameCycle();
+
+
+
+
